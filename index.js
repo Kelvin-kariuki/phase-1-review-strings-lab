e600f10ea0c508bbd083921e9aafd56110c3d569
kelvin-kariuki <Kariuki405@gmail.com>
2022-06-06 14:12:42 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = "Kelvin Kariuki";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `${welcomeMessage.toLocaleUpperCase()}`;
+
+const shortGreeting = `${welcomeMessage.slice(0,7)}, ${currentUser.slice(0,1)}!`
+
